cc68925dd986ac011c6c453e04990e6ea9ca2e7b
royhandf <royhandaffa14@gmail.com>
2022-10-28 06:28:55 +0700

    menambahkan deskripsi pada artikel

--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -1,30 +1,39 @@
 List articles = [
   {
+    'title': 'Pantai Teluk Asmara',
+    'timeUploaded': '26 Oktober 2022',
+    'description': 'Pantai Teluk Asmara adalah sebuah pantai yang terletak di Kabupaten Malang, Jawa Timur.'
+        'Pantai Teluk Asmara kerap dijuluki Raja Ampat karena memiliki geografis yang mirip dengan destinasi wisata di Papua Barat itu.'
+        'Pantai ini memiliki pasir yang putih dan warna laut yang biru jernih.'
+        'Selain itu, pantai ini juga memiliki hamparan pulau-pulau kecil yang bisa dilihat dari ketinggian dengan menaiki anak tangga.',
+    'image': 'teluk-asmara.jpg',
+  },
+  {
     'title': 'Gunung Bromo',
     'timeUploaded': '26 Oktober 2022',
-    'description':
-        'Gunung Bromo adalah sebuah gunung berapi aktif di Jawa Timur, Indonesia. Gunung ini terletak di Tengger Semeru National Park, di sebelah utara Gunung Semeru. Gunung ini memiliki ketinggian 2.329 mdpl dan merupakan bagian dari kompleks vulkanik Tengger.',
+    'description': 
+        'Gunung Bromo adalah sebuah gunung berapi aktif di Jawa Timur. '
+        'Gunung ini memiliki ketinggian 2.329 mdpl dan dan berada dalam empat wilayah kabupaten, yakni Kabupaten Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten Malang.'
+        'Gunung Bromo termasuk dalam kawasan Taman Nasional Bromo Tengger Semeru.',
     'image': 'bromo.jpg',
   },
   {
     'title': 'Taman Baluran',
     'timeUploaded': '26 Oktober 2022',
     'description':
-        'Taman Nasional Baluran adalah sebuah taman nasional di Jawa Timur, Indonesia. Taman nasional ini terletak di Kabupaten Jember, Kabupaten Banyuwangi, dan Kabupaten Bondowoso. Taman nasional ini memiliki luas 74.000 hektar dan merupakan taman nasional terluas di Jawa Timur.',
+        'Taman Nasional Baluran adalah sebuah taman nasional di Jawa Timur.'
+        'Taman nasional ini terletak di Kabupaten Jember, Kabupaten Banyuwangi, dan Kabupaten Bondowoso.' 
+        'Taman nasional ini memiliki luas 74.000 hektar dan merupakan taman nasional terluas di Jawa Timur.',
     'image': 'baluran.webp',
   },
   {
     'title': 'Tumpak Sewu',
     'timeUploaded': '26 Oktober 2022',
     'description':
-        'Tumpak Sewu adalah sebuah tempat wisata di Lumajang, Jawa Timur, Indonesia. Tumpak Sewu terletak di Desa Tumpak Sewu, Kecamatan Lumajang, Kabupaten Lumajang, Jawa Timur. Tumpak Sewu terletak di ketinggian 1.000 mdpl dan memiliki luas 1,5 hektar.',
+        'Tumpak Sewu atau yang disebut juga Coban Sewu adalah air terjun unik di Lumajang dikarenakan air terjun ini punya aliran air yang melebar seperti tirai.'
+        'Tumpak Sewu adalah air terjun yang memiliki ketinggian 120 meter yang bisa dinikmati atau dilihat dengan dua cara.'
+        'Yang pertama dilihat dari atas yang dikenal dengan sebutan panorama Coban Sewu.'
+        'Yang kedua dilihat dari dekat atau dari bawah langsung sehingga sensasi dingin dari air terkena langsung ke badan.',
     'image': 'tumpak-sewu.jpg',
-  },
-  {
-    'title': 'Pantai Teluk Asmara',
-    'timeUploaded': '26 Oktober 2022',
-    'description':
-        'Pantai Teluk Asmara adalah sebuah pantai yang terletak di Kabupaten Malang, Jawa Timur, Indonesia. Pantai Teluk Asmara kerap dijuluki Raja Ampat karena memiliki geografis yang mirip dengan destinasi wisata di Papua Barat itu. Pantai ini memiliki pasir yang putih dan warna laut yang biru jernih. Selain itu, pantai di Malang ini juga memiliki hamparan pulau-pulau kecil yang bisa dilihat dari ketinggian dengan menaiki anak tangga.',
-    'image': 'teluk-asmara.jpg',
   }
 ];
